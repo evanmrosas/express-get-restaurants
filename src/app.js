b736ce6b0f81041907bf18efb252817e0164f87a
evanmrosas <147665124+evanmrosas@users.noreply.github.com>
2024-09-24 16:04:41 -0500

    add the rest of the CRUD work for the endpoints

--- src/app.js
+++ src/app.js
@@ -4,16 +4,31 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/restaurants", async (req, res) => {
     const restaurants = await Restaurant.findAll()
     res.json(restaurants)
 })
-app.get("/restaurants/:id", async(req, res) =>{
+app.get("/restaurants/:id", async(req, res) => {
     const id = req.params.id
     const restaurant = await Restaurant.findByPk(id)
     res.json(restaurant)
 })
-
-
+app.post("/restaurants", async (req, res) => {
+    const newRest = await Restaurant.create(req.body)
+    res.json(newRest)
+})
+app.put("/restaurants/:id", async (req, res) => {
+    const id = req.params.id
+    const updateRest = await Restaurant.findByPk(id)
+    await updateRest.update(req.body)
+    res.json(updateRest)
+})
+app.delete("/restaurants/:id", async (req, res) => {
+    const id = req.params.id
+    const deleteRest = await Restaurant.findByPk(id)
+    await deleteRest.destroy()
+})
 module.exports = app;
