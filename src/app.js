a1a2005abfe4d0e1606ec57a79c88996986fe2ae
evanmrosas <evanmrosas@gmail.com>
2024-09-25 15:24:07 -0500

    finished adding the express routes

--- src/app.js
+++ src/app.js
@@ -7,24 +7,6 @@ const restaurantRoute = require("./routes/restaurant")
 
 app.use("/restaurants", restaurantRoute)
 
-app.get("/restaurants/:id", async(req, res) => {
-    const id = req.params.id
-    const restaurant = await Restaurant.findByPk(id)
-    res.json(restaurant)
-})
-app.post("/restaurants", async (req, res) => {
-    const newRest = await Restaurant.create(req.body)
-    res.json(newRest)
-})
-app.put("/restaurants/:id", async (req, res) => {
-    const id = req.params.id
-    const updateRest = await Restaurant.findByPk(id)
-    await updateRest.update(req.body)
-    res.json(updateRest)
-})
-app.delete("/restaurants/:id", async (req, res) => {
-    const id = req.params.id
-    const deleteRest = await Restaurant.findByPk(id)
-    await deleteRest.destroy()
-})
+app.use("/restaurants/:id", restaurantRoute)
+
 module.exports = app;
--- src/routes/restaurant.js
+++ src/routes/restaurant.js
@@ -1,14 +1,37 @@
 const express = require("express")
 const router = express.Router()
-const Restaurant = require(".../models/index")
-const db = require("../db/connection");
+const Restaurant = require("../../models/index")
+const db = require("../../db/connection");
 
 router.use(express.json())
 router.use(express.urlencoded({ extended: true }))
-
+// GET all
 router.get("/", async (req, res) => {
     const restaurants = await Restaurant.findAll()
     res.json(restaurants)
 })
-
+// GET one
+router.get("/:id", async(req, res) => {
+    const id = req.params.id
+    const restaurant = await Restaurant.findByPk(id)
+    res.json(restaurant)
+})
+// CREATE one
+router.post("/", async (req, res) => {
+    const newRest = await Restaurant.create(req.body)
+    res.json(newRest)
+})
+// UPDATE one
+router.put("/:id", async (req, res) => {
+    const id = req.params.id
+    const updateRest = await Restaurant.findByPk(id)
+    await updateRest.update(req.body)
+    res.json(updateRest)
+})
+// DELETE one
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id
+    const deleteRest = await Restaurant.findByPk(id)
+    await deleteRest.destroy()
+})
 module.exports = router;
